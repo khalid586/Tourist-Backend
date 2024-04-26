ccb95e0eb3ac312b0921e69be787a136fbf579e0
khalid586 <khalidcomilla58@gmail.com>
2024-04-26 19:44:52 +0600

    update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
 
 app.use(cors());
+
 app.use(express.json());
 
 
-
-const uri = "mongodb+srv://User:PL3VGW1BXEM4KGbU@cluster0.pf9u5p6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+require('dotenv').config();
+const uri = process.env.MONGO_URL
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,23 +22,42 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     await client.connect();
+    const database = client.db("userDatabase");
+    const userData = database.collection('users')
+
+    app.post('/users',async (req,res) =>{
+      const user = req.body;
+      const result = await userData.insertOne(user);
+      res.send(result);
+    })
+    
+    app.get('/users',async (req,res) =>{
+      const Users = userData.find();
+      const result = await Users.toArray();
+      res.send(result);
+    })
+    app.delete('/users/:id',async (req,res) =>{
+      const Id = req.params.id;
+      const deletedUser = {_id: new ObjectId(Id)};
+      const result = await userData.deleteOne(deletedUser);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
+  } catch{
+    console.error(error)
+  }
+  finally {
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req,res)=>{
-    res.send('Done successfully!')
-})
-
-app.listen(PORT,()=>{
-    console.log(`App running at port ${PORT}`);
+app.listen(port,()=>{
+    console.log(`App running at port ${port}`);
 })
